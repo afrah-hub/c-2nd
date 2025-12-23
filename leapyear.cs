7ee546f8217eb7f70aa268b861fb71ad456fbad4
afrah-hub <afrahkabeer328@gmail.com>
2025-12-23 17:08:07 +0530

    Add files via upload

---
+++ leapyear.cs
@@ -0,0 +1,14 @@
+﻿int year = 2024;
+
+if (year % 4 == 0 && year % 100 != 0)
+{
+    Console.WriteLine("Leap year");
+}
+else if (year % 400 == 0)
+{
+    Console.WriteLine("Leap year");
+}
+else
+{
+    Console.WriteLine("Not a leap year");
+}
