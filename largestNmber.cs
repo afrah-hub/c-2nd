41934a19de4a414f3db7c24e59f61d2194e9971c
afrah-hub <afrahkabeer328@gmail.com>
2025-12-23 17:03:16 +0530

    Add files via upload

---
+++ largestNmber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+{
+      int a = 10, b = 25, c = 15;
+
+if (a >= b && a >= c)
+{
+    Console.WriteLine("Largest number is " + a);
+}
+else if (b >= a && b >= c)
+{
+    Console.WriteLine("Largest number is " + b);
+}
+else
+{
+    Console.WriteLine("Largest number is " + c);
+}
+
+
+
+    }
+}   
