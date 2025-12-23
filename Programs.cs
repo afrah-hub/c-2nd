fc26356a152ea0dcac0a8be54ee5babcf44b0892
afrah-hub <afrahkabeer328@gmail.com>
2025-12-23 17:20:41 +0530

    Add files via upload

---
+++ Programs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(
+                new string(' ', n - i) +
+                new string('*', 2 * i - 1)
+            );
+        }
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+            Console.WriteLine(
+                new string(' ', n - i) +
+                new string('*', 2 * i - 1)
+            );
+        }
+    }
+}
