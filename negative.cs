86c9a86f2135247c1a7eac78fd1ae0d7338188be
afrah-hub <afrahkabeer328@gmail.com>
2025-12-23 17:04:38 +0530

    Add files via upload

---
+++ negative.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+{int number = 5;
+
+if (number > 0)
+{
+    Console.WriteLine("Positive number");
+}
+else if (number < 0)
+{
+    Console.WriteLine("Negative number");
+}
+else
+{
+    Console.WriteLine("Zero");
+}
+
+
+
+    }
+}   
